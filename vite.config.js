59344471d345d3e9c9a76bd73ac3f7811112d24f
SUMIT gourav <sumit.gourav280800@gmail.com>
2025-04-12 14:12:48 -0700

    Update vite.config.js and package.json for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Sumit01-tech.githun.io",
+  base: '/Portfolio-Website/',  // Adjust this to reflect the subfolder for your project
+  build: {
+    outDir: 'dist',  // Ensure the build outputs to the 'dist' folder
+  },
 });
