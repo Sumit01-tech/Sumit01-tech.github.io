540882afa0b6b08a11c55c1e4b20ebf7954676a6
SUMIT gourav <sumit.gourav280800@gmail.com>
2025-08-08 13:39:45 -0700

    Portfolio Website

--- Portfolio-Website/src/components/Nav.jsx
+++ Portfolio-Website/src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Nav.css";
-import Sumit_gourav_resume from "../files/Sumit-gourav-resume.pdf";
+import resume from "../files/Sumit-gourav-resume.pdf";
 
 const Nav = () => {
     const [isActive, setIsActive] = useState(false);
@@ -18,7 +18,7 @@ const Nav = () => {
         window.open("https://drive.google.com/file/d/1ENWoXPLQjKBQut0IG6nNbvmfjYpQqadA/view?usp=sharing", "_blank");
 
         const link = document.createElement("a");
-        link.href = Sumit_gourav_resume;
+        link.href = resume;
         link.setAttribute("download", "Sumit-gourav-resume.pdf");
         link.setAttribute("id", "resume-link-1");
         document.body.appendChild(link);
